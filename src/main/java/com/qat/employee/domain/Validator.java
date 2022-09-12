3cf964debfe50c95f6a90e22b855c5f1b21f565e
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-09-12 15:20:04 -0300

    refact: Validator

--- src/main/java/com/qat/employee/domain/Validator.java
+++ src/main/java/com/qat/employee/domain/Validator.java
@@ -9,7 +9,13 @@ public abstract class Validator{
   public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
       Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-  public abstract Boolean validate();
+  public abstract void checkValidState();
+
+  public Boolean validate(){
+    checkValidState();
+    return getErrors().isEmpty();
+  }
+
   private List<ValidationError> errors;
 
   public Validator() {
@@ -22,6 +28,24 @@ public abstract class Validator{
 
   public void addError(String field, String message) {
     errors.add(ValidationError.of(field, message));
+  }
+
+  public void required(String field, String value) {
+    if (value == null || value.isEmpty()) {
+      addError(field, field + " is required");
+    }
+  }
+
+  public void maxLength(String field, String value, int maxLength) {
+    if(value != null && value.length() > maxLength) {
+      addError(field, field + " must be less than " + maxLength + " characters");
+    }
+  }
+
+  public void email(String field, String value) {
+    if(value != null && !VALID_EMAIL_ADDRESS_REGEX.matcher(value).find()) {
+      addError(field, field + " is not valid");
+    }
   }
 
 }
--- src/main/java/com/qat/employee/domain/employee/BAC/EmployeeValidator.java
+++ src/main/java/com/qat/employee/domain/employee/BAC/EmployeeValidator.java
@@ -12,44 +12,24 @@ public class EmployeeValidator extends Validator {
   }
 
   @Override
-  public Boolean validate() {
+  public void checkValidState() {
 
-    if (employee.getName() == null || employee.getName().isEmpty()) {
-      addError("name", "Name is required");
-    }
+    required("name", employee.getName());
 
-    if(employee.getName() != null && employee.getName().length() > 50) {
-      addError("name", "Name must be less than 50 characters");
-    }
+    maxLength("name", employee.getName(), 50);
 
-    if (employee.getPhone() == null || employee.getPhone().isEmpty()) {
-      addError("phone", "Phone is required");
-    }
+    required("phone", employee.getPhone());
 
-    if(employee.getPhone() != null && employee.getPhone().length() > 20) {
-      addError("phone", "Phone must be less than 20 characters");
-    }
+    maxLength("phone", employee.getPhone(), 20);
 
-    if (employee.getEmail() == null || employee.getEmail().isEmpty()) {
-      addError("email", "Email is required");
-    }
+    required("email", employee.getEmail());
 
-    if(employee.getEmail() != null && employee.getEmail().length() > 50) {
-      addError("email", "Email must be less than 50 characters");
-    }
+    maxLength("email", employee.getEmail(), 50);
 
-    if (employee.getBranch() == null || employee.getBranch().isEmpty()) {
-      addError("branch", "Branch is required");
-    }
+    email("email", employee.getEmail());
 
-    if(employee.getBranch() != null && employee.getBranch().length() > 50) {
-      addError("branch", "Branch must be less than 50 characters");
-    }
+    required("branch", employee.getBranch());
 
-    if(employee.getEmail() != null && !VALID_EMAIL_ADDRESS_REGEX.matcher(employee.getEmail()).find()) {
-      addError("email", "Email is not valid");
-    }
-
-    return getErrors().isEmpty();
+    maxLength("branch", employee.getBranch(), 30);
   }
 }
